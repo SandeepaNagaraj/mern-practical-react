83c44f0aedd69e8771eb732f421ace69b084923a
SandeepaNagaraj <sandeepanagaraj4@gmail.com>
2023-06-12 14:30:44 +0530

    -Initial is done to save the file structure

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import HolidayApiComponent from './component/HolidayApiComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HolidayApiComponent/>
+    </div>
+  );
+}
+export default App;
